04f1f09306f0d9d0b42da136ab5f3218dd2e5d17
akashgiricse <akashgiricse@gmail.com>
2017-12-20 18:08:38 +0530

    Join the matches into a string for the clipboard

--- PhoneNumberAndEmailAddressExtractor/phoneAndEmail.py
+++ PhoneNumberAndEmailAddressExtractor/phoneAndEmail.py
@@ -34,3 +34,13 @@ for groups in phoneRegex.findall(text):
 
 for groups in emailRegex.findall(text):
 	matches.append(groups[0])
+
+
+# Copy results to the clipboard.
+
+if len(matches) > 0:
+	pyperclip.copy('\n'.join(matches))
+	print('Copied to clipboard: ')
+	print('\n'.join(matches))
+else:
+	print('No phone number or email addresses found.')
